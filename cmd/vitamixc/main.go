58d771e72f75ea38e1b73acdbe07a7f6b80f8df7
Petar Maymounkov <petar@tumblr.com>
2012-05-27 00:13:16 -0400

    recurse through subdirectories

--- cmd/vitamixc/main.go
+++ cmd/vitamixc/main.go
@@ -41,39 +41,16 @@ func main() {
 	if len(os.Args) != 3 {
 		usage()
 	}
-	src, tgt := os.Args[1], os.Args[2]
+	src, dest := os.Args[1], os.Args[2]
 	srcInfo, err := os.Stat(src)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Problem accessing source (%s)\n", err)
 		os.Exit(1)
 	}
 	if srcInfo.IsDir() {
-		// Verify that tgt is an existing directory
-		tgtInfo, err := os.Stat(tgt)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Problem accessing target (%s)\n", err)
+		if err := processDir(src, dest); err != nil {
+			fmt.Fprintf(os.Stderr, "Problem processing directory (%s)\n", err)
 			os.Exit(1)
-		}
-		if !tgtInfo.IsDir() {
-			fmt.Fprintf(os.Stderr, "Target must be an existing directory\n")
-			os.Exit(1)
-		}
-
-		fileSet := token.NewFileSet()
-		pkgs, err := parser.ParseDir(fileSet, src, FilterGoFiles, parser.ParseComments)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Problem parsing directory (%s)\n", err)
-			os.Exit(1)
-		}
-		for _, pkg := range pkgs {
-			for _, fileFile := range pkg.Files {
-				RewriteFile(fileSet, fileFile)
-				position := fileSet.Position(fileFile.Package)
-				if err = PrintToFile(path.Join(tgt, position.Filename), fileSet, fileFile); err != nil {
-					fmt.Fprintf(os.Stderr, "Problem determining source filename (%s)", err)
-					os.Exit(1)
-				}
-			}
 		}
 	} else {
 		fileSet := token.NewFileSet()
@@ -83,9 +60,64 @@ func main() {
 			os.Exit(1)
 		}
 		RewriteFile(fileSet, file)
-		if err = PrintToFile(tgt, fileSet, file); err != nil {
+		if err = PrintToFile(dest, fileSet, file); err != nil {
 			os.Exit(1)
 		}
 	}
 
+}
+
+func processDir(src, dest string) error {
+	fmt.Printf("Rewriting directory %s ——> %s\n", src, dest)
+
+	// Make destination directory if it doesn't exist
+	if err := os.MkdirAll(dest, 0755); err != nil {
+		fmt.Fprintf(os.Stderr, "Target directory cannot be created (%s).\n", err)
+		return err
+	}
+
+	// Parse source directory
+	fileSet := token.NewFileSet()
+	pkgs, err := parser.ParseDir(fileSet, src, FilterGoFiles, parser.ParseComments)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Problem parsing directory (%s)\n", err)
+		return err
+	}
+
+	// In every package, rewrite every file
+	for _, pkg := range pkgs {
+		for _, fileFile := range pkg.Files {
+			RewriteFile(fileSet, fileFile)
+			position := fileSet.Position(fileFile.Package)
+			_, filename := path.Split(position.Filename)
+			fmt.Printf("  %s ==> %s\n", filename, path.Join(dest, filename))
+			if err = PrintToFile(path.Join(dest, filename), fileSet, fileFile); err != nil {
+				fmt.Fprintf(os.Stderr, "Problem determining source filename (%s)", err)
+				os.Exit(1)
+			}
+		}
+	}
+
+	// Recurse the subdirectories of src
+	srcDir, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer srcDir.Close()
+
+	for { 
+		fi_, err := srcDir.Readdir(10)
+		for _, fi := range fi_ {
+			if !fi.IsDir() {
+				continue
+			}
+			if err := processDir(path.Join(src, fi.Name()), path.Join(dest, fi.Name())); err != nil {
+				return err
+			}
+		}
+		if err != nil {
+			break
+		}
+	}
+	return nil
 }
